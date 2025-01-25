87f8d0fef01e96a025f273172b1f1098d61072e7
Aleksandra Sokolova <asokolova.incoming@fh-salzburg.ac.at>
2025-01-26 00:56:53 +0100

    new changes 3

--- js/readMore.js
+++ js/readMore.js
@@ -1,10 +1,9 @@
 export function setupReadMoreModals() {
-    // Get all modal triggers
+
     const modalTriggers = document.querySelectorAll('.open-modal');
     const modals = document.querySelectorAll('.modal');
     const closeButtons = document.querySelectorAll('.close-modal');
 
-    // Open the correct modal when a trigger is clicked
     modalTriggers.forEach(trigger => {
         trigger.addEventListener('click', (e) => {
             e.preventDefault();
@@ -16,14 +15,13 @@ export function setupReadMoreModals() {
         });
     });
 
-    // Close the modal when the close button is clicked
+    
     closeButtons.forEach(button => {
         button.addEventListener('click', () => {
             button.closest('.modal').classList.remove('show');
         });
     });
 
-    // Close the modal when clicking outside the content
     modals.forEach(modal => {
         modal.addEventListener('click', (e) => {
             if (e.target === modal) {
